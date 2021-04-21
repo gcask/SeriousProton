2c4032a274d22ab7b517ac0dbbcee7fccb75ca8e
gcask <53709079+gcask@users.noreply.github.com>
2021-04-21 19:35:26 -0400

    Clear depth and color in a single pass

--- src/SFML/Graphics/Implementation.cpp
+++ src/SFML/Graphics/Implementation.cpp
@@ -1347,7 +1347,7 @@ namespace sf
     {
         ScopedRenderTarget guard{ this };
         glClearColor(color.r / 255.f, color.g / 255.f, color.b / 255.f, color.a / 255.f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
     void RenderTarget::draw(const Drawable& drawable, const RenderStates& states)
